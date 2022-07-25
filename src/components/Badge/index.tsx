ac5c20846c4c6ef85c4366de976caba9d0a6d32a
aaqib qadeer <aaqibqs@gmail.com>
2022-07-25 18:26:26 +0500

    remove counter

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,12 +1,8 @@
-import Counter from "../Counter";
-
 export const Badge = ({ counterNumber, text1, text2, className }) => {
   return (
     <div className={`numbers ${className}`}>
       <div className="wrapper">
-        <h3>
-          <Counter end={counterNumber} />
-        </h3>
+        <h3>{counterNumber}</h3>
         <span className="name">
           {text1}
           <br />
